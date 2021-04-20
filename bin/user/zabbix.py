b28f8c1c98d0e6131ea18269f4800c24f2966f3b
Marc Pignat <marc@pignat.org>
2021-04-20 09:35:45 +0200

    update to python3, tested with weewx 4.5.1

--- bin/user/zabbix.py
+++ bin/user/zabbix.py
@@ -7,7 +7,7 @@
 # \url     https://github.com/RandomReaper/weewx-zabbix
 #
 #############################################################################
-#
+# 
 # Copyright 2018 Marc Pignat
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,68 +24,48 @@
 #############################################################################
 
 from datetime import datetime
-import time
 import os
 
 import weeutil.weeutil
 import weewx.engine
 import weewx.units
+import logging
 
 from subprocess import Popen, PIPE, STDOUT
 
-import syslog
-
-def logmsg(dst, msg):
-    syslog.syslog(dst, 'zabbix: %s' % msg)
-
-def logdbg(msg):
-    logmsg(syslog.LOG_DEBUG, msg)
-
-def loginf(msg):
-    logmsg(syslog.LOG_INFO, msg)
-
-def logerr(msg):
-    logmsg(syslog.LOG_ERR, msg)
+log = logging.getLogger(__name__)
 
 class Zabbix(weewx.engine.StdService):
     def __init__(self, engine, config_dict):
         super(Zabbix, self).__init__(engine, config_dict)
 
-    loginf("init")
-    conf = config_dict['ZABBIX']
-    self.enable = weeutil.weeutil.to_bool(conf.get('enable', False))
-    self.zabbix_sender = conf.get('zabbix_sender', '/usr/bin/zabbix_sender')
-    self.prefix = conf.get('prefix', 'weewx_')
-    self.server = conf.get('server', '127.0.0.1')
-    self.host = conf.get('host', 'weewx-host')
-    self.send_interval = float(conf.get('send_interval', 0))
+        log.info("init")
+        conf = config_dict['ZABBIX']
+        self.enable = weeutil.weeutil.to_bool(conf.get('enable', False))
+        self.zabbix_sender = conf.get('zabbix_sender', '/usr/bin/zabbix_sender')
+        self.prefix = conf.get('prefix', 'weewx_')
+        self.server = conf.get('server', '127.0.0.1')
+        self.host = conf.get('host', 'weewx-host')
 
-    self.last_time = None
+        log.debug("self.enable=" + str(self.enable))
+        log.debug("self.zabbix_sender=" + self.zabbix_sender)
+        log.debug("self.prefix=" + self.prefix)
+        log.debug("self.server=" + self.server)
+        log.debug("self.host=" + self.host)
 
-    logdbg("self.enable=" + str(self.enable))
-    logdbg("self.zabbix_sender=" + self.zabbix_sender)
-    logdbg("self.prefix=" + self.prefix)
-    logdbg("self.server=" + self.server)
-    logdbg("self.host=" + self.host)
-    logdbg("self.send_interval=" + str(self.send_interval))
-
-    if self.enable:
-        self.bind(weewx.NEW_LOOP_PACKET, self.loop)
+        if self.enable:
+	        self.bind(weewx.NEW_LOOP_PACKET, self.loop)
 
     def loop(self, event):
-        logdbg("loop data:")
-        if self.send_interval > 0 and self.last_time != None and time.time() - self.last_time < self.send_interval:
-            logdbg("Ignoring packet. Next update in " + str(self.send_interval - (time.time() - self.last_time)) + "s")
-            return
-        self.last_time = time.time()
+        log.debug("loop data:")
         s = ""
         for key,value in event.packet.items():
             l=self.host + " " + self.prefix+key + " " + str(value) + "\n"
             s+=l
-            logdbg(l)
+            log.debug(l)
 
         c = [self.zabbix_sender, "-z", self.server, "-i", "-"]
-        logdbg("command line : " + str(c))
+        log.debug("command line : " + str(c))
         p = Popen(c, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
         sender_stdout = p.communicate(input=s.encode())[0]
-        loginf(self.zabbix_sender + " result: " +sender_stdout.decode())
+        log.debug(self.zabbix_sender + " result: " +sender_stdout.decode())
